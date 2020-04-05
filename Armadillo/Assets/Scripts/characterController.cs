dc8040c28bbfabc40f0f3aef6dbaee22210e2bde
entropy98 <entropy@Ada.localdomain>
2020-04-04 22:10:23 -0400

    player controller and animation

---
+++ Armadillo/Assets/Scripts/characterController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+
+	NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if(Input.GetKey(KeyCode.D))agent.Move(1,0,0);
+    	else if(Input.GetKey(KeyCode.A))agent.Move(-1,0,0);
+    }
+}
